eb5836010f7d23b6d382533423bada5f67575020
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2024-01-22 12:00:31 +0900

    initial commit development

---
+++ AntiProcrastinator/AntiProcrastinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AntiProcrastinator
+//
+//  Created by Артем Чжен on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
