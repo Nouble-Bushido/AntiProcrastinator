dcef2188a20eca32e38fe5be0ea65fb8304f94ae
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2024-02-08 11:54:19 +0900

    added MainViewController

--- AntiProcrastinator/AntiProcrastinator/Classes/Presentation/Main/MainViewController.swift
+++ AntiProcrastinator/AntiProcrastinator/Classes/Presentation/Main/MainViewController.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    private lazy var mainView = MainView()
     
+    override func loadView() {
+        super.loadView()
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        actionButtons()
+    }
+}
+
+//MARK: Private
+private extension MainViewController {
+    func actionButtons() {
+        mainView.infoButton.addTarget(self, action: #selector(pressInfoButton), for: .touchUpInside)
+        mainView.addTaskButton.addTarget(self, action: #selector(pressAddTaskButton), for: .touchUpInside)
+    }
+    
+    @objc func pressInfoButton() {
+        let vc = InfoViewController()
+        vc.modalPresentationStyle = .overFullScreen
+        UIApplication.shared.keyWindow?.rootViewController = vc
+    }
+    
+    @objc func pressAddTaskButton() {
+        
+    }
 }
