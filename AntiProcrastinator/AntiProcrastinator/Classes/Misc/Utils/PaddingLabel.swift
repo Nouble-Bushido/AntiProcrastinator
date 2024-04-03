fdcd2b55359c140e76037f9a8812efe6b6addfe7
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2024-04-03 11:57:44 +0900

    added paddingBottom

--- AntiProcrastinator/AntiProcrastinator/Classes/Misc/Utils/PaddingLabel.swift
+++ AntiProcrastinator/AntiProcrastinator/Classes/Misc/Utils/PaddingLabel.swift
@@ -10,9 +10,10 @@ import UIKit
 class PaddingLabel: UILabel {
     var paddingLeft: CGFloat = 20.scale
     var paddingRight: CGFloat = 25.scale
+    var paddingBottom: CGFloat = 0.scale
 
     override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets(top: 0, left: paddingLeft, bottom: 0, right: paddingRight)
+        let insets = UIEdgeInsets(top: 0, left: paddingLeft, bottom: paddingBottom, right: paddingRight)
         super.drawText(in: rect.inset(by: insets))
     }
 
